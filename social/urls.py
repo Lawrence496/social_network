899d41f00cd2e682d2227833c3f88b88ee96f3c2
Anyalechi <lawrenceanyalechi@gmail.com>
2021-05-02 16:11:29 -0700

    'Initial'

---
+++ social/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from .views import PostListView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView, ProfileView, ProfileEditView, AddFollower, RemoveFollower, AddLike, DisLike, UserSearch, ListFollowers, AddCommentLike, AddCommentDisLike, CommentReplyView, ListThreads, CreateThread, ThreadView, CreateMessage
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='post-list'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
+    path('post/edit/<int:pk>', PostEditView.as_view(), name='post-edit'),
+    path('post/delete/<int:pk>', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:post_pk>/comment/delete/<int:pk>', CommentDeleteView.as_view(), name='comment-delete'),
+    path('post/<int:pk>/like', AddLike.as_view(), name='like'),
+    path('post/<int:pk>/dislike', DisLike.as_view(), name='dislike'),
+    path('post/<int:post_pk>/comment/<int:pk>/like', AddCommentLike.as_view(), name='comment-like'),
+    path('post/<int:post_pk>/comment/<int:pk>/dislike', AddCommentDisLike.as_view(), name='comment-dislike'),
+    path('post/<int:post_pk>/comment/<int:pk>/reply', CommentReplyView.as_view(), name='comment-reply'),
+    path('profile/<int:pk>', ProfileView.as_view(), name='profile'),
+    path('profile/edit/<int:pk>', ProfileEditView.as_view(), name='profile-edit'),
+    path('profile/<int:pk>/followers/add', AddFollower.as_view(), name='add-follower'),
+    path('profile/<int:pk>/followers/remove', RemoveFollower.as_view(), name='remove-follower'),
+    path('profile/<int:pk>/followers', ListFollowers.as_view(), name='followers-list'),
+    path('search/', UserSearch.as_view(), name='profile-search'),
+    path('inbox/', ListThreads.as_view(), name='inbox'),
+    path('inbox/create-thread', CreateThread.as_view(), name='create-thread'),
+    path('inbox/<int:pk>/', ThreadView.as_view(), name='thread'),
+    path('inbox/<int:pk>/create-message/', CreateMessage.as_view(), name='create-message'),
+]
